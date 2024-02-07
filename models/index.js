fdb59ec2ff7e9afe3fff8ee2065be3c1256d695c
mwg396 <michael.graham1029@gmail.com>
2024-02-06 20:06:43 -0600

    Updated models and routes

--- models/index.js
+++ models/index.js
@@ -5,6 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+  onDelete: 'CASCADE',
+});
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
+
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
+
 
 // Categories have many Products
 
